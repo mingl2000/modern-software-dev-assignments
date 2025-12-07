b89d5b04b0e0750002a9d04f081eaa72078b2825
mingl2000 <mingl2000@gmail.com>
2025-12-07 12:43:08 -0800

    k_shot_prompting.py works

--- week1/k_shot_prompting.py
+++ week1/k_shot_prompting.py
@@ -7,7 +7,40 @@ load_dotenv()
 NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+#YOUR_SYSTEM_PROMPT = "You are a prefect assistant that reverses words accurately. Check your work by reversing the word again to ensure you get the input string."
+YOUR_SYSTEM_PROMPT ="""
+You reverse a word by mapping each character position to its reversed position.
+
+For any word W = w1 w2 w3 ... wn  
+you MUST output:  wn ... w3 w2 w1
+
+To ensure correctness, follow these exact examples:
+
+apple
+=> elppa
+
+robot
+=> tobor
+
+coffee
+=> eeffoc
+
+matrix
+=> xirtam
+
+httpstatus
+=> sutatsptth
+
+RULES:
+- Output MUST be only the reversed characters.
+- NO explanation.
+- NO sentences.
+- NO punctuation.
+- NO extra spaces.
+- NO arrow, no labels.
+- Only the reversed string.
+
+"""
 
 USER_PROMPT = """
 Reverse the order of letters in the following word. Only output the reversed word, no other text:
@@ -27,6 +60,7 @@ def test_your_prompt(system_prompt: str) -> bool:
         print(f"Running test {idx + 1} of {NUM_RUNS_TIMES}")
         response = chat(
             model="mistral-nemo:12b",
+            #model="llama3.1:8b",
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": USER_PROMPT},
